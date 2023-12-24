54468717594e53e66f8f467d12b2ff78242bd036
satyam-seth <satyam1998.1998@gmail.com>
2023-12-25 00:08:23 +0530

    Display Task Result

--- Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/urls.py
+++ Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('contact', views.contact, name='contact'),
+    path('result/<str:task_id>', views.check_result, name='check_result'),
 ]
--- Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/views.py
+++ Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from myceleryproject.celery import add
 from myapp.tasks import sub
+from celery.result import AsyncResult
 
 # # Enqueue Task using delay()
 # def index(request):
@@ -12,14 +13,25 @@ from myapp.tasks import sub
 #     print('Result 2: ', result2)
 #     return render(request, 'myapp/home.html')
 
-# Enqueue Task using apply_async()
+# # Enqueue Task using apply_async()
+# def index(request):
+#     print("Results: ")
+#     result1 = add.apply_async(args=[10, 20])
+#     print('Result 1: ', result1)
+#     result2 = sub.apply_async(args=[30, 20])
+#     print('Result 2: ', result2)
+#     return render(request, 'myapp/home.html')
+
+# Display addition value after task execution
 def index(request):
-    print("Results: ")
-    result1 = add.apply_async(args=[10, 20])
-    print('Result 1: ', result1)
-    result2 = sub.apply_async(args=[30, 20])
-    print('Result 2: ', result2)
-    return render(request, 'myapp/home.html')
+    result = add.delay(10, 30)
+    return render(request, "myapp/home.html", {'result': result})
+
+
+def check_result(request, task_id):
+    # Retrieve the task result using the task_id
+    result = AsyncResult(task_id)
+    return render(request, 'myapp/result.html', {'result': result})
 
 def about(request):
     return render(request, 'myapp/about.html')
