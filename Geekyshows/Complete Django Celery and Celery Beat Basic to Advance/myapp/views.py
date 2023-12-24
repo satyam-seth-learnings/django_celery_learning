b76bc53602e8322a1385714bfe8d207fe2ad8dbd
satyam-seth <satyam1998.1998@gmail.com>
2023-12-24 23:56:19 +0530

    Enqueue Task using apply_async

--- Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/views.py
+++ Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 from myceleryproject.celery import add
 from myapp.tasks import sub
 
-# Enqueue Task using delay()
+# # Enqueue Task using delay()
+# def index(request):
+#     print("Results: ")
+#     # result1 = add(10, 20)
+#     result1 = add.delay(10, 20)
+#     print('Result 1: ', result1)
+#     result2 = sub.delay(30, 20)
+#     print('Result 2: ', result2)
+#     return render(request, 'myapp/home.html')
+
+# Enqueue Task using apply_async()
 def index(request):
     print("Results: ")
-    # result1 = add(10, 20)
-    result1 = add.delay(10, 20)
+    result1 = add.apply_async(args=[10, 20])
     print('Result 1: ', result1)
-    result2 = sub.delay(30, 20)
+    result2 = sub.apply_async(args=[30, 20])
     print('Result 2: ', result2)
     return render(request, 'myapp/home.html')
 
