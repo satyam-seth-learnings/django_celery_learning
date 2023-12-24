27e7ab8712c37da47e96b09f0cadd5f389f424dd
satyam-seth <satyam1998.1998@gmail.com>
2023-12-24 19:45:05 +0530

    Celery Settings to define Redis Message Broker

--- Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myceleryproject/settings.py
+++ Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myceleryproject/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Celery Settings
+CELERY_BROKER_URL = "redis:127.0.0.1:6379/0"
+CELERY_TIMEZONE = "Asia/Kolkata"
