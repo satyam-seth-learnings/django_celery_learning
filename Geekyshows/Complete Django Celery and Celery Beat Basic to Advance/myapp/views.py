5f5d6dc2a83b1fad721948db043ca39e678b149f
satyam-seth <satyam1998.1998@gmail.com>
2023-12-24 23:51:44 +0530

    Enqueue Shared Task then Start Process

--- Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/views.py
+++ Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from myceleryproject.celery import add
+from myapp.tasks import sub
 
 # Enqueue Task using delay()
 def index(request):
@@ -7,6 +8,8 @@ def index(request):
     # result1 = add(10, 20)
     result1 = add.delay(10, 20)
     print('Result 1: ', result1)
+    result2 = sub.delay(30, 20)
+    print('Result 2: ', result2)
     return render(request, 'myapp/home.html')
 
 def about(request):
