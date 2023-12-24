ed7cf1e61d41a8bbb526783a20018bc847616b0b
satyam-seth <satyam1998.1998@gmail.com>
2023-12-24 20:21:15 +0530

    install myapp in settings.py and create home, about and contact page

--- Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/views.py
+++ Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    print("Results: ")
+    return render(request, 'myapp/home.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def contact(request):
+    return render(request, 'myapp/contact.html')
