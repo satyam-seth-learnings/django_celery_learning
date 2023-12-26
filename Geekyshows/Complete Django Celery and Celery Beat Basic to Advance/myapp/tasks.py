1be8859ebfa60bef94ea38684ff5f2f9d7ee6748
satyam-seth <satyam1998.1998@gmail.com>
2023-12-26 23:31:03 +0530

    Schedule Periodic Task Programmatically

--- Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/tasks.py
+++ Geekyshows/Complete Django Celery and Celery Beat Basic to Advance/myapp/tasks.py
@@ -1,5 +1,7 @@
 from celery import shared_task
 from time import sleep
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
+import json
 
 @shared_task
 def sub(x, y):
@@ -15,3 +17,22 @@ def clear_session_cache(id):
 def clear_redis_data(key):
     print(f"Redis Data Cleared: {key}")
     return key
+
+
+@shared_task
+def clear_rabbitmq_data(key):
+    print(f"RabbitMQ Data Cleared: {key}")
+    return key
+
+# Create Schedule every 30 seconds
+schedule, created = IntervalSchedule.objects.get_or_create(
+    every=30,
+    period=IntervalSchedule.SECONDS,
+)
+# Schedule the periodic task programmatically
+PeriodicTask.objects.get_or_create(
+    name='Clear RabbitMQ Periodic Task',
+    task='myapp.tasks.clear_rabbitmq_data',
+    interval=schedule,
+    args=json.dumps(['hello']),  # Pass the arguments to the task as a JSON-encoded list
+)
